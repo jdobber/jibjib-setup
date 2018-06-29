2e5fa6c18674a7ced4da80124641f9459476c1a0
n365 <n365@MFN0006.naturkundemuseum-berlin.de>
2018-06-29 17:54:39 +0200

    changed port and auto detect container id

--- init_mongodb.sh
+++ init_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+CONTAINER_ID=$(docker ps --filter "name=jibjib-mongo" --format="{{.ID}}")
 echo "Init MongoDB in container $CONTAINER_ID "
 docker exec $CONTAINER_ID bash /initdb/setup.sh
